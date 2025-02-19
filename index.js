faf9befc05a5ff09a03f01f0600d278030df6e0c
Ryan Simpson <ryzizn@gmail.com>
2025-02-19 00:35:02 -0500

    create log event emitter

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('starting...');
+const logEvents = require('./logEvents');
+
+const eventEmitter = require('events');
+
+class MyEmitter extends eventEmitter {}
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+  // Emit event
+
+  myEmitter.emit('log', 'Log event emitted!');
+}, 2000);
