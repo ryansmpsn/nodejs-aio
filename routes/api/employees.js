994b3e80856f68cd76b86548a53ab957bd487ded
Ryan Simpson <ryzizn@gmail.com>
2025-02-19 13:57:20 -0500

    cleanup server & unused files

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,33 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const data = {};
-data.employees = require('../../data/employees.json');
+const employeesController = require('../../controllers/employeesController');
 
 router
   .route('/')
-  .get((req, res) => {
-    res.json(data.employees);
-  })
-  .post((req, res) => {
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname
-    });
-  })
-  .put((req, res) => {
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname
-    });
-  })
-  .delete((req, res) => {
-    res.json({ id: req.body.id });
-  });
+  .get(employeesController.getAllEmployees)
+  .post(employeesController.createNewEmployee)
+  .put(employeesController.updateEmployee)
+  .delete(employeesController.deleteEmployee);
 
-router.route('/:id').get((req, res) => {
-  res.json({ id: req.params.id });
-});
+router.route('/:id').get(employeesController.getEmployee);
 
 // To create a RESTful API for managing employees using Express.js, you can follow the structure provided in the snippets. Below is a complete example of how you can set up the `employees` route to handle various HTTP methods like GET, POST, PUT, and DELETE.
 
--- server.js
+++ server.js
@@ -2,25 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const { logger, logEvents } = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
 const errorHandler = require('./middleware/errorHandler');
 
 // custom middleware logger
 app.use(logger);
-
-// Cross Origin Resource Sharing
-const whitelist = ['https://www.yoursite.com', 'http://localhost:3000'];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200
-};
 
 app.use(cors(corsOptions));
 
@@ -29,11 +17,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 // route handlers
 app.use('/', require('./routes/root.js'));
-app.use('/', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('/*', (req, res) => {
